e9148c686bb9207009685d3664b5feb41128f1d2
yukun1120 <116103371+yukun1120@users.noreply.github.com>
2024-11-15 22:18:21 +0900

    仮データのセット

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,24 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+let blogPosts = [
+  {
+    id:"1",
+    title:"blog1",
+    content:"blogblog1"
+  },
+  {
+    id:"2",
+    title:"blog2",
+    content:"blogblog2"
+  },
+  {
+    id:"3",
+    title:"blog3",
+    content:"blogblog3"
+  }
+]
+
 app.get('/', (c) => {
   return c.text('hello hono')
 })
